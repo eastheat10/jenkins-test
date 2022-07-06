3f2355384d0730744c684fc10171b0c948e1dced
윤동열 <eastheat10@gmail.com>
2022-07-06 14:32:00 +0900

    initial commit

---
+++ src/test/java/com/example/demo/ControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerTest {
+
+    @Autowired
+    Controller controller;
+
+    @Test
+    void index() {
+        assertEquals(controller.index(), "index");
+    }
+
+}
